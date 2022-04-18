2a74236e7440574f1dd33b47e045d651d27a58af
weraleto <52561921+weraleto@users.noreply.github.com>
2022-04-18 23:37:58 +0300

    added "up" button

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,6 +16,11 @@
         :show-textarea="true"
         v-if="$store.state.showApplyForm"
         @close="$store.commit('setShowModal', {key: 'showApplyForm', val: false})" />
+
+
+    <div class="up-button" @click="scrollToTop" v-if="showUpButton">
+        <img src="~assets/img/arrow.svg" alt="Наверх страницы">
+    </div>
   </div>
   
 </template>
@@ -27,6 +32,18 @@ import FormModal from '@/components/modal-form'
 import {mapState} from 'vuex'
 export default {
     components: {Navbar, Footer, FormModal},
+    data: () => {
+        return {
+            showUpButton: false
+        }
+    },
+    mounted() {
+        let ctx = this
+        window.addEventListener('scroll', function(){
+            console.log(window.pageYOffset > 5450)
+            ctx.showUpButton = window.pageYOffset > 5450
+        })
+    },
     computed: {
         ...mapState([
             'showContactForm',
@@ -34,6 +51,11 @@ export default {
         ]),
         isModalOpened() {
             return this.showContactForm || this.showApplyForm
+        }
+    },
+    methods: {
+        scrollToTop() {
+            window.scrollTo(0, 0)
         }
     },
     watch: {
@@ -51,6 +73,8 @@ export default {
 
 <style lang="scss" >
 
+@import '@/assets/scss/_variables.scss';
+
 .app {
     display: flex;
     flex-direction: column;
@@ -61,6 +85,27 @@ export default {
 
     max-width: 100vw;
     overflow: hidden;
+}
+
+.up-button {
+    width: 60px;
+    height: 60px;
+    position: fixed;
+    background-color: $--main-white;
+    bottom: 60px;
+    left: 50%;
+    transform: translate(-50%, 0);
+    cursor: pointer;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid $--main-gray;
+
+    img {
+        width: 22px;
+        transform: rotate(-135deg);
+    }
 }
 
 </style>
