83ce7983964da315add8a5da04ac33a3fafeab00
weraleto <52561921+weraleto@users.noreply.github.com>
2023-03-27 20:41:45 +0300

    test script

--- layouts/default.vue
+++ layouts/default.vue
@@ -60,6 +60,9 @@ export default {
             this.$refs.navbar.closeAllDropdowns()
         }
     },
+    mounted() {
+        (function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b16224776/crm/site_button/loader_2_3yeodk.js');
+    }
 
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -59,9 +59,6 @@ export default {
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'manifest', href: '/site.webmanifest' },
     ],
-    script: [
-      { innerHTML: "(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b16224776/crm/site_button/loader_2_3yeodk.js');", body: true }
-    ]
   },
   serverMiddleware: [
     '~/api/contact.js'
