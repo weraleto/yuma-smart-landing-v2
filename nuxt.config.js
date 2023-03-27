2b47d875217a1fbb6f35e0bf944d6d3bac9793b4
weraleto <52561921+weraleto@users.noreply.github.com>
2023-03-27 20:32:53 +0300

    fix script

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
       { rel: 'manifest', href: '/site.webmanifest' },
     ],
     script: [
-      { innerHTML: `(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b16224776/crm/site_button/loader_2_3yeodk.js');`, body: true }
+      { innerHTML: "(function(w,d,u){var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);})(window,document,'https://cdn-ru.bitrix24.ru/b16224776/crm/site_button/loader_2_3yeodk.js');", body: true }
     ]
   },
   serverMiddleware: [
