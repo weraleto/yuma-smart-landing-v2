49b248dbe815c3c58e6393e8cdc784a7d6f50baf
weraleto <52561921+weraleto@users.noreply.github.com>
2023-06-14 15:44:53 +0300

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -63,7 +63,12 @@ export default {
     script: [{
       innerHTML: `(function ab(){ var request = new XMLHttpRequest(); request.open('GET', "https://scripts.botfaqtor.ru/one/39763", false); request.send(); if(request.status == 200) eval(request.responseText); })();`,
       type: 'text/javascript'
-    }]
+    },
+    {
+        src: '//code.jivo.ru/widget/h0nnJuwWNk',
+        async: true,
+        type: 'text/javascript'
+      }]
   },
   serverMiddleware: [
     '~/api/contact.js'
