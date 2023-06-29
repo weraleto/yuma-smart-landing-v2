ed1bf2dbae1b0053473433c1760dd26d3946774e
weraleto <52561921+weraleto@users.noreply.github.com>
2023-06-29 18:12:57 +0300

    added googletagmanager

--- nuxt.config.js
+++ nuxt.config.js
@@ -59,16 +59,30 @@ export default {
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'manifest', href: '/site.webmanifest' },
     ],
-    __dangerouslyDisableSanitizers: ['script'],
+    __dangerouslyDisableSanitizers: ['script', 'noscript'],
     script: [{
       innerHTML: `(function ab(){ var request = new XMLHttpRequest(); request.open('GET', "https://scripts.botfaqtor.ru/one/39763", false); request.send(); if(request.status == 200) eval(request.responseText); })();`,
       type: 'text/javascript'
     },
     {
+      innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-MXDKZWT');`
+    },
+    {
         src: '//code.jivo.ru/widget/h0nnJuwWNk',
         async: true,
         type: 'text/javascript'
-      }]
+      }],
+    noscript: [
+      {
+        innerHTML: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MXDKZWT"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        body: true
+      },
+    ]
   },
   serverMiddleware: [
     '~/api/contact.js'
